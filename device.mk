704d2c123a7f3e7936b99b2e501663426974988b
micky387 <mickaelsaibi@free.fr>
2020-12-09 13:01:53 +0100

    rog3: Import overlay from sm8250-common
    Change-Id: Ie4fef10c2bd8947eb0a6ccd3cd2c0c43adb4317c

--- device.mk
+++ device.mk
@@ -36,6 +36,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration_ZS661KS.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/audio_policy_volumes_ZS661KS.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_volumes_ZS661KS.xml
 
+# Frameworks
+PRODUCT_PACKAGES += \
+    FrameworksResOverlay
+
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/goodix_ts.idc:system/usr/idc/goodix_ts.idc \
